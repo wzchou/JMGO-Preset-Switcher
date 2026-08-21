08bd64dec79ffcfc8cfe5e5ce16b61d17b7df5d2
wzchou <wzchou@gmail.com>
2026-08-21 13:50:20 +0000

    Fix translucent key listener

--- app/src/main/java/com/wzchou/jmgopresetswitcher/KeyListenerActivity.kt
+++ app/src/main/java/com/wzchou/jmgopresetswitcher/KeyListenerActivity.kt
@@ -4,26 +4,21 @@ import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.WindowManager
+import android.view.View
 
 class KeyListenerActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        window.setBackgroundDrawableResource(android.R.color.transparent)
-        window.addFlags(
-            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
-            WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-        )
-
-        val params = window.attributes
-        params.width = 1
-        params.height = 1
-        params.alpha = 0.01f
-        window.attributes = params
-
+        window.setDimAmount(0f)
         window.decorView.setBackgroundColor(Color.TRANSPARENT)
+
+        setContentView(
+            View(this).apply {
+                setBackgroundColor(Color.TRANSPARENT)
+            }
+        )
     }
 
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
