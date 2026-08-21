583afa33e5248e1e9436cf73919f6d717fa0bf7e
wzchou <wzchou@gmail.com>
2026-08-21 03:25:19 +0000

    Align Java and Kotlin JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,4 +14,13 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
